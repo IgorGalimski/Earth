26622e9ca6572b4b6ca182f452982d3961c2fa5a
IgorGalimski <igorg@fm-games.com>
2021-12-18 09:19:00 +0200

    Add light and camera

--- Earth/ViewController.swift
+++ Earth/ViewController.swift
@@ -6,13 +6,87 @@
 //
 
 import UIKit
+import SceneKit
+import AVKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SCNCameraControllerDelegate
+{
+    var scene: SCNScene?
+    var sceneView: SCNView?
 
-    override func viewDidLoad() {
+    var audioPlayer: AVAudioPlayer?
+    
+    var sunAndLightPosition: (x: CGFloat, y: CGFloat, z: CGFloat) = (-20, .zero, .zero)
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
+        SetupSceneView()
+        SetupLight()
+        SetupCamera()
     }
     
+    func SetupSceneView()
+    {
+        sceneView = SCNView(frame: view.bounds)
+        scene = SCNScene()
+        
+        sceneView?.scene = scene
+        sceneView?.backgroundColor = .black
+        
+        view.addSubview(sceneView!)
+        
+        sceneView?.defaultCameraController.delegate = self
+        sceneView?.allowsCameraControl = true
+        sceneView?.cameraControlConfiguration.allowsTranslation = true
+        sceneView?.cameraControlConfiguration.panSensitivity = 1.0
+        sceneView?.defaultCameraController.interactionMode = .fly
+    }
+    
+    func SetupLight()
+    {
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light?.type = .omni
+        lightNode.position = SCNVector3(sunAndLightPosition.x, sunAndLightPosition.y, sunAndLightPosition.z)
+        
+        if scene != nil
+        {
+            scene?.rootNode.addChildNode(lightNode)
+        }
+    }
+    
+    func SetupCamera()
+    {
+        let cameraNode = SCNNode()
+        cameraNode.name = "CameraNode"
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3(30, .zero, 9)
+        cameraNode.rotation = SCNVector4(.zero, 1, .zero, -3.14)
+        if scene != nil
+        {
+            scene?.rootNode.addChildNode(cameraNode)
+        }
+    }
+    
+    func GeneratePlanet(nodeName: String, textureName: String?, isPlanet: Bool,
+                        isEarth: Bool, hasRing: Bool, animatitionDuration: Double?,
+                        radius: CGFloat, x: CGFloat, y: CGFloat, z: CGFloat)
+    {
+        let planetNode = SCNNode()
+        planetNode.geometry = SCNSphere(radius: radius)
+        planetNode.name = nodeName
+        planetNode.position = SCNVector3(x, y, z)
+        
+        if isEarth
+        {
+            
+        }
+        else
+        {
+            guard let planetTexture = textureName else { return }
+        }
+    }
 }
 
